58f5701118693d905156308154b97771309628a7
Jason Lam <jasonlamv-t@hotmail.com>
2023-11-10 11:15:10 +0800

    chore: introduce nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,19 @@ import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './common/config/app.config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService, PrismaService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) { }
+
+  async validateUser(username: string, password: string) {
+    const user = await this.usersService.user({ username });
+    if (!user) throw NotFoundException;
+
+    user.encryptedPassword =
+  }
+}
