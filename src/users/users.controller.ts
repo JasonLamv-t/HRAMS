de556d3eb9594c0540759ece8825dc966ced85f5
Jason Lam <jasonlamv-t@hotmail.com>
2023-11-15 14:09:19 +0800

    update: add interface to test the function of global auth guard and the jwt

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
+
+  @Get(':username')
+  async getUsers(@Param('username') username: string) {
+    return username;
+  }
 }
